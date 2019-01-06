e999caceb324cbaf8b85fb57ecc2b48a688dba4c
yashshah7197 <yash.shah7197@live.com>
2019-01-06 11:39:26 +0530

    Remove global and export flags while setting PATH in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,10 @@ set fish_greeting
 set -x GPG_TTY (tty)
 
 # Add different PATHs required for various commands
-set -gx PATH "/usr/local/sbin" $PATH 
-set -gx PATH /usr/local/miniconda3/bin $PATH
-set -gx PATH "/usr/local/opt/openssl/bin" $PATH
-set -gx PATH ~/Library/Android/sdk/platform-tools $PATH
+set PATH /usr/local/sbin $PATH
+set PATH /usr/local/miniconda3/bin $PATH
+set PATH /usr/local/opt/openssl/bin $PATH
+set PATH ~/Library/Android/sdk/platform-tools $PATH
 
 # Source conda.fish for 'conda activate' to properly work with fish shell
 source (conda info --root)/etc/fish/conf.d/conda.fish
