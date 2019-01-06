1e13e603d6692868657b8865ba3c9e0cb23d0889
yashshah7197 <yash.shah7197@live.com>
2019-01-06 12:54:12 +0530

    Add Flutter to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ set -x GPG_TTY (tty)
 set PATH /usr/local/sbin $PATH
 set PATH /usr/local/miniconda3/bin $PATH
 set PATH /usr/local/opt/openssl/bin $PATH
+set PATH ~/Library/Flutter/flutter/bin $PATH
 set PATH ~/Library/Android/sdk/platform-tools $PATH
 
 # Source conda.fish for 'conda activate' to properly work with fish shell
