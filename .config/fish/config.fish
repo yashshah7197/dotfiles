70dd7cfc426c611e0e41fdd119f00b8e4c6b7cba
yashshah7197 <yash.shah7197@live.com>
2019-10-13 01:22:51 +0530

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,16 +4,9 @@ set fish_greeting
 # GPG environment variable
 set -x GPG_TTY (tty)
 
-# Android environment variables
-set -x ANDROID_SDK_ROOT /usr/local/share/android-sdk
-set -x ANDROID_NDK_HOME {$ANDROID_SDK_ROOT}/ndk-bundle
-
 # Add different PATHs required for various commands
 set PATH /usr/local/sbin $PATH
 set PATH /usr/local/miniconda3/bin $PATH
-set PATH /usr/local/opt/openssl/bin $PATH
-set PATH {$ANDROID_SDK_ROOT}/platform-tools $PATH
-set PATH {$HOME}/Library/Flutter/sdk/bin $PATH
 
 # Source conda.fish for 'conda activate' to properly work with fish shell
 source (conda info --root)/etc/fish/conf.d/conda.fish
